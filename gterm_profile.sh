b3177777e8c7b1e248ddcd8c80c2baccbc7ab146
Joshua Griffin Dunn <joshua.g.dunn@gmail.com>
2017-10-25 23:15:21 -0400

    reverted for ubuntu 14.04

--- gterm_profile.sh
+++ gterm_profile.sh
@@ -6,15 +6,12 @@
 # made after some code snippet from the internet that I need to attribute
 
 # get profile names
-#PROFILE_PATH=/apps/gnome-terminal/profiles
-PROFILE_PATH=/org/gnome/terminal/legacy/profiles:/
+PROFILE_PATH=/apps/gnome-terminal/profiles
 profile_names=""
-#for folder in $(gconftool --all-dirs $PROFILE_PATH); do
-for folder in $(dconf list $PROFILE_PATH | grep -v list); do
-    #profile_names="${profile_names} $(gconftool --get ${folder}/visible_name)";
-    profile_names="${profile_names} $(dconf read ${PROFILE_PATH}${folder}visible-name)";
+for folder in $(gconftool --all-dirs $PROFILE_PATH); do
+    profile_names="${profile_names} $(gconftool --get ${folder}/visible_name)";
 done
-profile_names=($(echo $profile_names | sed -e "s/'//g"))
+profile_names=($profile_names)
 
 # launch terminal using code snippet pilfered from somewhere online
 gnome-terminal --window-with-profile ${profile_names[$((RANDOM%${#profile_names[@]}))]} $@
